36c892c67d582c2ec9aa67f0774e19d3af7f5548
Rosalioalfre2 <rosalioalfre2@gmail.com>
2021-12-30 10:30:04 -0600

    Validando campos de formulario

--- js/01 - querySelector.js
+++ js/01 - querySelector.js
@@ -46,15 +46,77 @@ navegacion.appendChild(enlaceAReady);
 
 //Eventos
 
-/* 
+/*
 const btnEnviar = document.querySelector('#btn-enviar');
- btnEnviar.addEventListener('click',function(evento){
-     console.log("Validando formulario");
-     console.log('/n',evento);
-     evento.preventDefault();
- })
+btnEnviar.addEventListener('click', function (evento) {
+    console.log("Validando formulario");
+    console.log('/n', evento);
+    evento.preventDefault();
+})
 
- window.addEventListener('scroll',function(evento){
-     console.log('Escroleando....');
- }) 
- */
+window.addEventListener('scroll', function (evento) {
+    console.log('Escroleando....');
+})
+*/
+
+
+let datos = {
+    campo__nombre : '',
+    campo__email : '',
+    campo__mensaje : ''
+}
+
+const nombre = document.querySelector("#campo__nombre");
+const email = document.querySelector("#campo__email");
+const mensaje = document.querySelector("#campo__mensaje");
+const formulario = document.querySelector(".formulario");
+
+nombre.addEventListener('input',leerTexto);
+email.addEventListener('input',leerTexto);
+mensaje.addEventListener('input',leerTexto);
+
+
+/* formulario.addEventListener('submit', validarFormulario); */
+formulario.addEventListener('submit', validarFormulario);
+
+function validarFormulario(evento){
+    evento.preventDefault();
+
+    const {campo__nombre, campo__email, campo__mensaje} = datos;
+
+    if(campo__nombre === '' || campo__email === '' || campo__mensaje === '' ){
+        mensajeError("Todos los campos son obligatorios");
+        return;
+    }
+
+    mensajeCorrecto("Formulario valido")
+}
+
+function leerTexto(e){
+    datos[e.target.id] = e.target.value;
+    console.log(datos)
+}
+
+function mensajeError(mensaje){
+    const mensajeError = document.createElement("P");
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('mensaje-error');
+
+    formulario.appendChild(mensajeError);
+
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 5000);
+}
+
+function mensajeCorrecto(mensaje){
+    const mensajeCorrecto = document.createElement("P");
+    mensajeCorrecto.textContent = mensaje;
+    mensajeCorrecto.classList.add('mensaje-correcto');
+
+    formulario.appendChild(mensajeCorrecto);
+
+    setTimeout(() => {
+        mensajeCorrecto.remove();
+    }, 5000);
+}
